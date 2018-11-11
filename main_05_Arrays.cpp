204c0d0f99bddddf1bd299d407fb3d2ddc2e4139
Suryakiran M. George <25682779+suryakiranmg@users.noreply.github.com>
2018-11-10 23:53:47 -0500

    Update main_05_Arrays.cpp

--- main_05_Arrays.cpp
+++ main_05_Arrays.cpp
@@ -90,3 +90,56 @@ int main() {
     return 0;
 }
  
+
+//---------------------------------------------------------------------------------------//
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <limits>
+ 
+int main() {
+    
+    // ----- PROBLEM CALCULATOR -----
+    // Enter calculation (ex. 5 + 6) : 10 - 6
+    // 10.0 - 6.0 = 4.0
+    
+    double dbNum1 = 0, dbNum2 = 0;
+    std::string sCalc = "";
+    std::vector<std::string> vecsCalc; 
+    
+    std::cout << "Enter calculation (ex. 5 + 6): ";
+    getline(std::cin, sCalc);
+    
+    std::stringstream ss(sCalc);
+    std::string sIndivStr;
+    char cSpace = ' ';
+    
+    while(getline(ss, sIndivStr, cSpace)){
+        vecsCalc.push_back(sIndivStr);
+    }
+    
+    dbNum1 = std::stoi(vecsCalc[0]);
+    dbNum2 = std::stoi(vecsCalc[2]);
+    std::string operation = vecsCalc[1];
+    
+    if (operation == "+"){
+        printf("%.1f + %.1f = %.1f\n", dbNum1, dbNum2,
+                (dbNum1 + dbNum2));
+    } else if (operation == "-"){
+        printf("%.1f - %.1f = %.1f\n", dbNum1, dbNum2,
+                (dbNum1 - dbNum2));
+    } else if (operation == "*"){
+        printf("%.1f * %.1f = %.1f\n", dbNum1, dbNum2,
+                (dbNum1 * dbNum2));
+    } else if (operation == "/"){
+        printf("%.1f / %.1f = %.1f\n", dbNum1, dbNum2,
+                (dbNum1 / dbNum2));
+    } else {
+        std::cout << "Please enter only +, -, *, or /\n";
+    }
+    
+    // ----- END PROBLEM CALCULATOR -----
+    return 0;
+}
