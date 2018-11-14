603cc4747490a8b5fd6eab7176bd4dddff1c7adc
Suryakiran M. George <25682779+suryakiranmg@users.noreply.github.com>
2018-11-13 23:53:35 -0500

    Update main_06_PointersFunctions.cpp

--- main_06_PointersFunctions.cpp
+++ main_06_PointersFunctions.cpp
@@ -1,34 +1,37 @@
-// ---------- C++ Tutorial 3  ----------
+// ---------- C++ Tutorial 3  ----------Pointers & Functions-----------------------//
+    #include <numeric>
+    int main()
+    {
+      std::vector<int> myVec(10);
+      // Generates incremental values starting at 0 and places them in the vector provided
+      // begin points at the 1st index and end points at the last in the vector
+      std::iota(std::begin(myVec), std::end(myVec), 0);
+
+      // We can output the vector using a standard for loop
+      for(int i = 0; i < myVec.size(); ++i)
+         {
+           std::cout << myVec[i] << "\n";
+         }
+      
+      // Abbreviated FOR loop - auto decides data type based on value,y is assigned each value in vector incrementing the index by 1 each time through the loop
+      for(auto y: myVec) std::cout << y << "\n";
+
+      // Find if a value is even / odd with modulus
+      int val = 8;
+      if((val % 2) == 0)
+         {
+             std::cout << val << " is Even\n";
+         } 
+      else 
+         {
+             std::cout << val << " is Odd\n";
+         }
+
+        return 0;
+    }
  
-std::vector<int> myVec(10);
-    
-    // Generates incremental values starting at 0 and places
-    // them in the vector provided
-    
-    // begin points at the 1st index and end points at the last
-    // in the vector
-    std::iota(std::begin(myVec), std::end(myVec), 0);
-    
-    // We can output the vector using a standard for loop
-    for(int i = 0; i < myVec.size(); ++i){
-        std::cout << myVec[i] << "\n";
-    }
-    
-    // An abbreviated for loop is also available
-    // auto determines the data type based on the value
-    // y is assigned each value in the vector incrementing
-    // the index by 1 each time through the loop
-    for(auto y: myVec) std::cout << y << "\n";
-    
-    // Find if a value is even / odd with modulus
-    int val = 8;
-    if((val % 2) == 0){
-        std::cout << val << " is Even\n";
-    } else {
-        std::cout << val << " is Odd\n";
-    }
-    
-    // ----- PROBLEM : GENERATE VECTOR (1 - 10) AND PRINT EVENS ----- 
+
+// ----- PROBLEM : GENERATE VECTOR (1 - 10) AND PRINT EVENS ----- 
     
     std::vector<int> myVec2(10);
     std::iota(std::begin(myVec2), std::end(myVec2), 1);
@@ -39,9 +42,9 @@ std::vector<int> myVec(10);
             std::cout << y << "\n";
     }
     
-    // ----- END OF PROBLEM -----
+// ----- END OF PROBLEM -----------------------------------------
     
-    #include <cstdlib>
+#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <vector>
