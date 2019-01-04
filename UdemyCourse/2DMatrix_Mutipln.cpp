5bf8354763a40ee3967fe571d01b14955e9fceb5
Suryakiran M. George <25682779+suryakiranmg@users.noreply.github.com>
2019-01-04 20:26:33 +0530

    Update 2DMatrix_Mutipln.cpp

--- UdemyCourse/2DMatrix_Mutipln.cpp
+++ UdemyCourse/2DMatrix_Mutipln.cpp
@@ -13,48 +13,48 @@ cout << "Enter rows and columns for second matrix: ";
 cin >> r2 >> c2;
 
 if (c1!=r2)
-{
-cout<<“Cant be Multiplied”;
-return 0;
-}
+    {
+    cout<<“Cant be Multiplied”;
+    return 0;
+    }
 
 // Storing elements of first matrix.
 cout << endl << "Enter elements of matrix 1:" << endl;
 for(i = 0; i < r1; ++i)
 for(j = 0; j < c1; ++j)
-{
-cout << "Enter element a" << i + 1 << j + 1 << " : ";
-cin >> a[i][j];
-}
+    {
+    cout << "Enter element a" << i + 1 << j + 1 << " : ";
+    cin >> a[i][j];
+    }
 
 // Storing elements of second matrix.
 cout << endl << "Enter elements of matrix 2:" << endl;
 for(i = 0; i < r2; ++i)
 for(j = 0; j < c2; ++j)
-{
-cout << "Enter element b" << i + 1 << j + 1 << " : ";
-cin >> b[i][j];
-}
+    {
+    cout << "Enter element b" << i + 1 << j + 1 << " : ";
+    cin >> b[i][j];
+    }
 
 // Multiplying matrix a and b and storing in array mult.
 for(i = 0; i < r1; ++i)
 for(j = 0; j < c2; ++j)
-{
-mult[i][j]=0;
-for(k = 0; k < c1; ++k)
-{
-mult[i][j] += a[i][k] * b[k][j];
-}
-}
+    {
+      mult[i][j]=0;
+      for(k = 0; k < c1; ++k)
+      {
+      mult[i][j] += a[i][k] * b[k][j];
+      }
+    }
 
 // Displaying the multiplication of two matrix.
 cout << endl << "Output Matrix: " << endl;
 for(i = 0; i < r1; ++i)
 for(j = 0; j < c2; ++j)
-{
-cout << " " << mult[i][j];
-if(j == c2-1)
-cout << endl;
-}
+  {
+  cout << " " << mult[i][j];
+  if(j == c2-1)
+  cout << endl;
+  }
 return 0;
 }
