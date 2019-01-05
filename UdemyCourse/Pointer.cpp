e1cfa8b0587ee12482acd2251ff2b0ac92612aac
Suryakiran M. George <25682779+suryakiranmg@users.noreply.github.com>
2019-01-06 00:24:10 +0530

    Update Pointer.cpp

--- UdemyCourse/Pointer.cpp
+++ UdemyCourse/Pointer.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+using namesapce std;
+void display()
+{
+  cout<<"hello";
+}
 
+int main()
+{
+  int *p;
+  a = 10;
+  p = &a;
+  
+  //Function Pointer
+  void(*fp)(); // structure definition
+  fp = display; // initialisation
+  
+  
+  cout<<p; // gives the address stored in p 
+  cout<<*p; // gives the value stored in the address in p, ie 10
+  
+  
+  (*fp)(); //function call
+  
+  
+  
+}
