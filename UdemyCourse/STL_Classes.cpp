015e1ffa512a64bf875b7004edc2e45fa913acaa
Suryakiran M. George <25682779+suryakiranmg@users.noreply.github.com>
2019-01-09 13:22:53 +0530

    Create STL_Classes.cpp

---
+++ UdemyCourse/STL_Classes.cpp
@@ -0,0 +1,106 @@
+//--------------VECTOR--------------------------------//
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v={2,4,6,8,10};
+    v.push_back(20);
+    v.push_back(30);
+    v.pop_back();
+    
+    vector<int>::iterator i;
+    
+    cout<<"Using iterator"<<endl;
+    for(i=v.begin();i != v.end(); i++)
+        cout<<++*i<<endl;
+        //cout<<*i<<endl;
+        
+    cout<<"Using for_each_loop"<<endl;
+    for(auto x:v)
+        cout<<x<<endl;
+    }
+    
+    
+    
+//--------------LINKED LIST (Doubly)--------------------------------//
+#include<iostream>
+#include<list>
+using namespace std;
+
+int main()
+{
+    list<int> v={2,4,6,8,10};
+    v.push_back(20);
+    v.push_back(30);
+    v.pop_back();
+    
+    list<int>::iterator i;
+    
+    cout<<"Using iterator"<<endl;
+    for(i=v.begin();i != v.end(); i++)
+        cout<<++*i<<endl;
+
+    cout<<"Using for_each_loop"<<endl;
+    for(auto x:v)
+        cout<<x<<endl;
+    }
+        
+ //--------------FORWARD LIST (Singly)--------------------------------//       
+#include<iostream>
+#include<forward_list>
+using namespace std;
+
+int main()
+{
+    forward_list<int> v={2,4,6,8,10};
+    v.push_front(20);
+    v.push_front(30);
+   // v.pop_back(); //no push back in forward list
+    
+    forward_list<int>::iterator i;
+    
+    cout<<"Using iterator"<<endl;
+    for(i=v.begin();i != v.end(); i++)
+        cout<<++*i<<endl;
+
+    cout<<"Using for_each_loop"<<endl;
+    for(auto x:v)
+        cout<<x<<endl;
+    }
+        
+//--------------------------MAP(hash table)-------------------//        
+#include<iostream>
+#include<map>
+using namespace std;
+//~~~~~map is useful when we have to do frequent search on data
+
+int main()
+{
+    map<int,string> m;
+    m.insert(pair<int,string>(1,"Jhon"));
+    m.insert(pair<int,string>(2,"Ravi"));
+    m.insert(pair<int,string>(3,"Khan"));
+    m.insert(pair<int,string>(4,"Kiran"));
+    
+    map<int,string>::iterator itr;
+    
+    for(itr=m.begin();itr!=m.end();itr++)
+    { 
+      cout<<itr->first<<"  "<<itr->second<<endl;   
+    }
+    
+    cout<<endl;
+    //To search for a key
+    map<int,string>::iterator itr1;
+    itr1 = m.find(4);
+    cout<<"Value found is:"<<endl;
+    cout<<itr1->first<<"  "<<itr1->second<<endl;  
+  
+ }
+        
+        
+
+        
+        
