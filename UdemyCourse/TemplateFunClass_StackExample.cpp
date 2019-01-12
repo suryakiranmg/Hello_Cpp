b9bfe0d1a9e8f0b92ca74b54859e5a22fbd46782
Suryakiran M. George <25682779+suryakiranmg@users.noreply.github.com>
2019-01-12 12:48:21 +0530

    Update TemplateFunClass_StackExample.cpp

--- UdemyCourse/TemplateFunClass_StackExample.cpp
+++ UdemyCourse/TemplateFunClass_StackExample.cpp
@@ -1,1 +1,49 @@
 
+#include <iostream>
+using namespace std;
+
+template <class G >
+class Stack
+{
+    private:
+        G *stk;
+        int top;
+        int size;
+    public:
+        //constructor
+        Stack(int size)
+        {
+            this->size = size;
+            top = -1;
+            stk = new G[size];
+        }
+        //prototypes of facilitators
+        void push(G num);
+        G pop();
+};
+
+//Definitions of facilitators
+template <class G >
+void Stack<G> :: push(G num)
+{
+    if(top == size-1) cout<<"Stack Full"<<endl;
+    else stk[++top] = num;
+}
+
+template<class G>
+G Stack<G> :: pop()
+{
+    if(top == -1) cout<<"Stack Empty"<<endl;
+    else return(stk[top--]);
+}
+
+//main function
+int main()
+{
+    Stack<float> s(3);
+    s.push(2);s.push(5);
+    cout<<s.pop()<<endl;
+    cout<<s.pop()<<endl;    
+}
+
+
