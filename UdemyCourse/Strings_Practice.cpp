20ca09b25e42171cad500e37235004b3e65c97f6
Suryakiran Menachery George <25682779+suryakiranmg@users.noreply.github.com>
2020-04-10 19:45:59 -0500

    Update Strings_Practice.cpp

--- UdemyCourse/Strings_Practice.cpp
+++ UdemyCourse/Strings_Practice.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include<string>
-
 using namespace std;
 
+//-------------------Length of a String--------------------------------------------------//
 int main()
 {
-    //Length of a string
     string str = "Hello Suki";
     
     int count=0;
@@ -18,6 +17,27 @@ int main()
     
     cout<<"Lenth - using function1 : "<<str.length()<<endl;
     cout<<"Lenth - using function2 : "<<str.size()<<endl;
+    
+    return 0;
+}
+
+
+//-------------------Interchange Case ( lower to UPPER & back )------------------------//
+int main()
+{
+    string str = "Hello Suki 2020";
+  
+    for(int i=0; str[i] !='\0'; i++) //all lower
+    {
+        if(str[i]>=65 && str[i]<=90) cout<<char(str[i]+32);
+        else cout<<str[i];
+    }
+    cout<<endl;
+    for(int i=0; str[i] !='\0'; i++) //all upper
+    {
+        if(str[i]>=97 && str[i]<=122) cout<<char(str[i]-32);
+        else cout<<str[i];
+    }
     
     return 0;
 }
